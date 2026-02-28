48840f0b80a30299aeea34161803c80f674df249
Lukas Ceratti Agnese <lukelucanolightknowledge@gmail.com>
2026-02-27 21:32:26 -0300

    Metodo Abstrato

--- python-oop-api/stepapiopp/modelos/cardapio/item_cardapio.py
+++ python-oop-api/stepapiopp/modelos/cardapio/item_cardapio.py
@@ -1,7 +1,12 @@
-class ItemCardapio:
+from abc import ABC, abstractmethod
+class ItemCardapio(ABC):
     ## init é um método especial em Python que é chamado quando um objeto é criado a partir de uma classe. Ele é usado para inicializar os atributos do objeto.
     def __init__(self, nome: str, preco: float):
         ## Atributos privados
         self._nome = nome
         self._preco = preco
 
+    @abstractmethod
+    # O decorador @abstractmethod é usado para indicar que um método é abstrato, ou seja, que deve ser implementado por qualquer classe que herde da classe base. Ele é usado em conjunto com a classe ABC (Abstract Base Class) para criar classes abstratas em Python.
+    def aplicar_desconto(self):
+        pass
